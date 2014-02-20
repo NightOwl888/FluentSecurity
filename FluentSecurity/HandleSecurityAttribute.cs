7d38b20c65bfe23c1a895d420580df8e05e7688f
Shad Storhaug <shad@shadstorhaug.com>
2014-02-20 16:24:29 +0700

    - Fix for authorization of pages that are output cached so they are not served to unauthorized users.

--- FluentSecurity/HandleSecurityAttribute.cs
+++ FluentSecurity/HandleSecurityAttribute.cs
@@ -1,6 +1,8 @@
 using System;
 using System.ComponentModel;
+using System.Web;
 using System.Web.Mvc;
+using System.Web.Routing;
 using FluentSecurity.ServiceLocation;
 
 namespace FluentSecurity
@@ -8,6 +10,8 @@ namespace FluentSecurity
 	[AttributeUsage(AttributeTargets.Class, AllowMultiple = false, Inherited = true)]
 	public class HandleSecurityAttribute : AuthorizeAttribute, IAuthorizationFilter
 	{
+		internal ActionResult OverrideResult { get; private set; }
+		internal string ControllerName { get; private set; }
 		internal ISecurityHandler Handler { get; private set; }
 
 		public HandleSecurityAttribute() : this(ServiceLocator.Current.Resolve<ISecurityHandler>()) {}
@@ -17,16 +21,61 @@ namespace FluentSecurity
 			Handler = securityHandler;
 		}
 
-		public override void OnAuthorization(AuthorizationContext filterContext)
+		protected override bool AuthorizeCore(HttpContextBase httpContext)
 		{
-			var actionName = filterContext.ActionDescriptor.ActionName;
-			var controllerName = filterContext.ActionDescriptor.ControllerDescriptor.ControllerType.FullName;
+			if (httpContext == null)
+			{
+				throw new ArgumentNullException("httpContext");
+			}
+
+			var routeData = RouteTable.Routes.GetRouteData(httpContext);
+			var actionName = routeData.GetRequiredString("action");
 
 			var securityContext = SecurityContext.Current;
-			securityContext.Data.RouteValues = filterContext.RouteData.Values;
+			securityContext.Data.RouteValues = routeData.Values;
 
-			var overrideResult = Handler.HandleSecurityFor(controllerName, actionName, securityContext);
-			if (overrideResult != null) filterContext.Result = overrideResult;
+			OverrideResult = Handler.HandleSecurityFor(ControllerName, actionName, securityContext);
+
+			return (OverrideResult == null);
+		}
+
+		public override void OnAuthorization(AuthorizationContext filterContext)
+		{
+			if (filterContext == null)
+			{
+				throw new ArgumentNullException("filterContext");
+			}
+
+			ControllerName = filterContext.ActionDescriptor.ControllerDescriptor.ControllerType.FullName;
+
+			if (AuthorizeCore(filterContext.HttpContext))
+			{
+				// ** IMPORTANT **
+				// Since we're performing authorization at the action level, the authorization code runs
+				// after the output caching module. In the worst case this could allow an authorized user
+				// to cause the page to be cached, then an unauthorized user would later be served the
+				// cached page. We work around this by telling proxies not to cache the sensitive page,
+				// then we hook our custom authorization code into the caching mechanism so that we have
+				// the final say on whether a page should be served from the cache.
+
+				var cachePolicy = filterContext.HttpContext.Response.Cache;
+				cachePolicy.SetProxyMaxAge(new TimeSpan(0));
+				cachePolicy.AddValidationCallback(CacheValidateHandler, null /* data */);
+			}
+			else
+			{
+				HandleUnauthorizedRequest(filterContext);
+			}
+		}
+
+		protected override void HandleUnauthorizedRequest(AuthorizationContext filterContext)
+		{
+			filterContext.Result = OverrideResult;
+		}
+
+		private void CacheValidateHandler(HttpContext context, object data, ref HttpValidationStatus validationStatus)
+		{
+			validationStatus = OnCacheAuthorization(new HttpContextWrapper(context));
 		}
 
 		[Obsolete("Not applicable in this class.")]
